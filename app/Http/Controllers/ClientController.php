0d29a11db9c1cf56f8e627aa01b2ad2ee8a7bd39
jart1757 <jart1757@gmail.com>
2025-04-03 10:58:43 -0400

    corregido hora y clientes

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -4,20 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Client;
+use Illuminate\Support\Facades\DB;
 
 class ClientController extends Controller
 {
     public function search(Request $request)
-{
-    $search = $request->search;
+    {
+        $search = $request->search;
 
+        $clients = Client::where('name', 'like', "%{$search}%")
+            ->orWhere('identificacion', 'like', "%{$search}%")
+            ->orderBy(DB::raw('GREATEST(updated_at, created_at)'), 'desc') // Ordena por la fecha más reciente entre ambas
+            ->limit(5)
+            ->get();
     
-    $clients = Client::where('name', 'like', "%{$search}%")
-        ->orWhere('identificacion', 'like', "%{$search}%")
-        ->orderBy('name')
-        ->limit(10)
-        ->get();
+        return response()->json($clients);
 
-    return response()->json($clients);
-}
+    }
 }
